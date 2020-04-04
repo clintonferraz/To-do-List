e25239a50e39ffbb52f02b624e09afa323de8813
Clinton <clinton_ferraz@hotmail.com>
2020-04-04 16:58:15 -0300

    improving indent level

--- index.js
+++ index.js
@@ -6,12 +6,30 @@ let itemsContainer = document.querySelector('.todoItemsContainer');
 itemsContainer.appendChild(firstItem);  
 
 let addItem = (pressed_button) => {
-    let todoItem = document.createElement('div');
-    todoItem.classList.add('todoItem');
-    todoItem.innerHTML = document.getElementById('todoItemHTML').innerHTML;
-    todoItem.setAttribute('data-indentLevel',pressed_button.parentNode.getAttribute('data-indentLevel'));
-    todoItem.style.paddingLeft = 3*todoItem.getAttribute('data-indentLevel') + 'rem';
-    itemsContainer.insertBefore(todoItem, pressed_button.parentNode.nextElementSibling);
+    let newItem = document.createElement('div');
+    newItem.classList.add('todoItem');
+    newItem.innerHTML = document.getElementById('todoItemHTML').innerHTML;
+
+    let indentLevelOfnewItem = pressed_button.parentNode.getAttribute('data-indentLevel');
+    newItem.setAttribute('data-indentLevel', indentLevelOfnewItem);
+    
+    newItem.style.paddingLeft = 3*indentLevelOfnewItem + 'rem';
+
+    let refItemToInsertBefore = pressed_button.parentNode.nextElementSibling;
+    while(refItemToInsertBefore != null){
+        let indentLevelOfRefItem = refItemToInsertBefore.getAttribute('data-indentLevel');
+        if( parseInt(indentLevelOfRefItem) > parseInt(indentLevelOfnewItem) ){
+            refItemToInsertBefore = refItemToInsertBefore.nextElementSibling;
+            console.log('1');
+        }else{
+            console.log('2');
+            break;
+        }
+    }
+    
+
+
+    itemsContainer.insertBefore(newItem, refItemToInsertBefore);
 
 /*  if(itemsContainer.childElementCount > 3)
     itemsContainer.childNodes[2].remove(); */
@@ -28,6 +46,9 @@ let addSubItem = (pressed_button) => {
 
 let removeItem = (pressed_button) => {
     pressed_button.parentNode.remove();
+    
+
+
 };
 
 
