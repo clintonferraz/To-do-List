c0ffb17d01baca941240ca0ebfb3a01f0f5ca83a
Clinton <clinton_ferraz@hotmail.com>
2020-04-03 22:19:53 -0300

    added insert and remove functionalities

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 let todoItem = document.createElement('div');
 todoItem.classList.add('todoItem');
-todoItem.innerHTML = document.getElementById('todoItem').innerHTML;
+todoItem.innerHTML = document.getElementById('todoItemHTML').innerHTML;
 
 let itemsContainer = document.querySelector('.todoItemsContainer');
 itemsContainer.appendChild(todoItem);  
+
+let addItem = (pressed_button) => {
+    let todoItem = document.createElement('div');
+    todoItem.classList.add('todoItem');
+    todoItem.innerHTML = document.getElementById('todoItemHTML').innerHTML;
+    itemsContainer.insertBefore(todoItem, pressed_button.parentNode.nextElementSibling);
+
+/*     if(itemsContainer.childElementCount > 3)
+        itemsContainer.childNodes[2].remove(); */
+}
+
+let removeItem = (pressed_button) => {
+    pressed_button.parentNode.remove();
+    console.log(pressed_button);
+
+}
